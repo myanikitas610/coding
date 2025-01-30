ff6753860edbc7594d7194674dd9500f4574af25
Mya Nikitas <mnikitas@fordham.edu>
2025-01-30 15:19:16 -0500

    practice

--- c++/practice.cpp
+++ c++/practice.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 using namespace std;
 
+// 2d array charFreq 
+const int rows = 3;
+const int cols = 3; 
+
+int charFreq(char charArray[rows][cols], char target){
+    int count = 0;
+    for (int i = 0; i < rows; i++){
+        for (int j =  0; j < cols; j++){
+            if (charArray[i][j] == target){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
 int main(){
-    string username = "mya"; 
-    string password = "skye"; 
 
-    cout << "Username: ";
-    cin >> username;
-    cout << "Passwrod: ";
-    cin >> password;
+    char arr[rows][cols] = {{'a', 'b', 'c'},
+                            {'c', 'b', 'a'},
+                            {'a', 'a', 'a'}};
+
+    char target;
+    cout << "What character are you looking for?" << endl; 
+    cin >> target;
+
+    int freq = charFreq(arr, target);
+
+    cout << "There are " << freq << " occurences of " << target << " in the array" << endl;
 
 
-    if (username == "mya" && password == "skye")
-        cout << "You logged in successfully";
-    else
-        cout << "Wrong information try again";
 }
